646707898d0ad3ada873013e9b0edbcff02fb936
nathaaanbraud <nathan.braud24@gmail.com>
2024-11-08 17:11:09 +0100

    Ajout des models TODO : Reservation.ts

---
+++ src/app/model/Jeux.ts
@@ -0,0 +1,7 @@
+export interface Jeux {
+  id: number;
+  nom: string;
+  quantite: number;
+  description: string;
+  pointGeo: string;
+}
---
+++ src/app/model/Utilisateur.ts
@@ -0,0 +1,8 @@
+export interface Utilisateur {
+  id: number;
+  nom: string;
+  prenom: string;
+  mail: string;
+  password: string;
+  username: string;
+}

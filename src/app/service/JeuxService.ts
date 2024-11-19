7ebb7196c9b93096b298f64a1926a9de51255713
nathaaanbraud <nathan.braud24@gmail.com>
2024-11-19 11:51:17 +0100

    tentative pour le service jeux

--- src/app/service/JeuxService.ts
+++ src/app/service/JeuxService.ts
@@ -1,4 +1,6 @@
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Jeux} from "../model/Jeux";
 
 export class JeuxService {
   // Il vaut mieux les mettre dans un fichier de configuration
@@ -6,4 +8,14 @@ export class JeuxService {
   API_ENTITY_NAME : string = 'jeux'; // TODO : Vérifier si le nom de l'entité est correcte
 
   constructor(private readonly http: HttpClient) { }
+
+  // Récupérer un jeu par ID
+  getJeuxById(id: number): Observable<Jeux> {
+    return this.http.get<Jeux>(`${this.API_URL}/${id}`);
+  }
+
+  // Ajouter un jeu
+  addJeux(jeu: Jeux): Observable<Jeux> {
+    return this.http.post<Jeux>(this.API_URL, jeu);
+  }
 }

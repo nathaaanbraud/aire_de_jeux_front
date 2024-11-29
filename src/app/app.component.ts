01b329927e9eeb2337da82feac5fabac684e2675
Marin Boehm <marin.boehm@etu.univ-tours.fr>
2024-11-29 18:21:31 +0100

    blabala

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {TableauComponent} from "./components/tableau/tableau.component";
 import {MapComponent} from "./components/map/map.component";
 import {JeuxService} from "./service/JeuxService";
+import {FormulaireReservationComponent} from "./components/formulaire-reservation/formulaire-reservation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TableauComponent, MapComponent],
+  imports: [RouterOutlet, TableauComponent, MapComponent, FormulaireReservationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

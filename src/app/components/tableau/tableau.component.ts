11170e83cde9d798fdf83f4d92cfff065c639551
Ikramsk6 <ikram_souki@outlook.fr>
2024-12-10 15:09:47 +0100

    pagination du tableau

--- src/app/components/tableau/tableau.component.ts
+++ src/app/components/tableau/tableau.component.ts
@@ -1,46 +1,64 @@
-import {Component, OnInit} from '@angular/core';
-import {Jeux} from "../../model/Jeux";
-import {JeuxService} from "../../service/JeuxService";
-import {Observable} from "rxjs";
-import {NgForOf} from "@angular/common";
-import {Router, RouterLink, RouterLinkActive} from "@angular/router";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { Jeux } from '../../model/Jeux';
+import { JeuxService } from '../../service/JeuxService';
+import { Observable } from 'rxjs';
+import { NgForOf } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tableau',
   standalone: true,
   imports: [
     NgForOf,
-    RouterLink,
-    RouterLinkActive
+    MatPaginatorModule,
+    MatTableModule
   ],
   templateUrl: './tableau.component.html',
-  styleUrl: './tableau.component.css'
+  styleUrls: ['./tableau.component.css']
 })
 export class TableauComponent implements OnInit {
-  jeux!: Observable<Jeux[]>;
-  jeuxList: Jeux[]=[];
+  jeuxList: Jeux[] = [];
+  displayedColumns: string[] = ['id', 'nom', 'description', 'quantite', 'pointGeo'];
+  paginatedData: Jeux[] = [];
+  pageSize = 5; // Nombre d'éléments par page
+  currentPage = 0; // Page actuelle
+
   constructor(private JeuxService: JeuxService, private router: Router) {}
 
-  ngOnInit() : void {
+  ngOnInit(): void {
     this.JeuxService.getAllJeux().subscribe((data: Jeux[]) => {
       this.jeuxList = data;
+      this.updatePagination();
     });
-  };
-  // Redirige vers la route du formulaire avec l'ID
+  }
+
+  updatePagination(): void {
+    const startIndex = this.currentPage * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.paginatedData = this.jeuxList.slice(startIndex, endIndex);
+  }
+
+  onPageChange(event: any): void {
+    this.currentPage = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.updatePagination();
+  }
+
   navigateToFormulaire(id: number): void {
     if (id) {
       this.router.navigate([`/app-formulaire/${id}`]);
     } else {
       console.error('ID non valide pour la redirection');
-    }}
-  // Redirige vers la route du formulaire de réservation
+    }
+  }
+
   navigateToFormulaireReservation(): void {
     this.router.navigate(['/app-formulaire-reservation/']);
   }
-  // Redirige vers la route de la carte
+
   navigateToMap(): void {
     this.router.navigate(['/app-map/']);
   }
-
 }
-

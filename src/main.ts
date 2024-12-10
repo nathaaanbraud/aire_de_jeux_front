dec8bd09fcef29a73584a29f9c297bf3deeae9d2
leaaah2 <rissel.lea@gmail.com>
2024-12-10 16:02:46 +0100

    ca marche avec style pagination

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import {provideHttpClient} from "@angular/common/http";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes), provideHttpClient(),
+    provideRouter(routes), provideHttpClient(), provideAnimationsAsync(),
   ],
 });

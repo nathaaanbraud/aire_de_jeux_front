580cf41d5b0ab91ef85acca556f530a550d052b2
Marin Boehm <marin.boehm@etu.univ-tours.fr>
2024-12-15 17:49:20 +0100

    ajout du error intercepteur et des message d'erreur personnalisés

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import {provideHttpClient} from "@angular/common/http";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { errorInterceptor } from './app/service/interceptors/error.service';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes), provideHttpClient(), provideAnimationsAsync(),
+    provideRouter(routes), provideHttpClient(withInterceptors([errorInterceptor])), provideAnimationsAsync(),
+
   ],
 });
